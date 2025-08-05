0bb19bb63f4a3f945c54c3dff060d15c2f61cf05
Warren <warren@dubelyoo.com>
2025-08-05 15:50:57 -0500

    Updating

--- home.nix
+++ home.nix
@@ -69,30 +69,30 @@
       promptinit
       prompt adam1
       
-      zstyle ':completion:*' auto-description 'specify: %d'
-      zstyle ':completion:*' completer _expand _complete _correct _approximate
-      zstyle ':completion:*' format 'Completing %d'
-      zstyle ':completion:*' group-name '''
-      zstyle ':completion:*' menu select=2
-      eval "$(dircolors -b)"
-      zstyle ':completion:*:default' list-colors ''${(s.:.)LS_COLORS}
-      zstyle ':completion:*' list-colors '''
-      zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-      zstyle ':completion:*' matcher-list ''' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|      =*'
-      zstyle ':completion:*' menu select=long
-      zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-      zstyle ':completion:*' use-compctl false
-      zstyle ':completion:*' verbose true
+      # zstyle ':completion:*' auto-description 'specify: %d'
+      # zstyle ':completion:*' completer _expand _complete _correct _approximate
+      # zstyle ':completion:*' format 'Completing %d'
+      # zstyle ':completion:*' group-name '''
+      # zstyle ':completion:*' menu select=2
+      # eval "$(dircolors -b)"
+      # zstyle ':completion:*:default' list-colors ''${(s.:.)LS_COLORS}
+      # zstyle ':completion:*' list-colors '''
+      # zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+      # zstyle ':completion:*' matcher-list ''' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|      =*'
+      # zstyle ':completion:*' menu select=long
+      # zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+      # zstyle ':completion:*' use-compctl false
+      # zstyle ':completion:*' verbose true
       
-      zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
-      zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+      # zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+      # zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
       
       
       ### Added by Zinit's installer
       if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
-          print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F      {33}zdharma-continuum/zinit%F{220})…%f"
+          print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
           command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-          command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.      git" && \
+          command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
               print -P "%F{33} %F{34}Installation successful.%f%b" || \
               print -P "%F{160} The clone has failed.%f%b"
       fi
@@ -114,13 +114,17 @@
       export PATH=$PATH:/home/warren/.local/bin
       
       if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
-        eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/base.toml)"
+        eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/spaceship.omp.json)"
       fi
+
+      export PATH=$PATH:/home/warren/.local/bin
       
       ####
       # home-manager - initContent done.
       ####
     '';
+
+    dotDir = "${config.xdg.configHome}/zsh";
 
     sessionVariables = {
       EDITOR = "nvim";
